e0dcdaf3750ba2b72811ab8f9d772a8c5e2de5c1
Burin-Zhargal <papaev.b.n@gmail.com>
2023-07-27 10:36:13 +0300

    change pub and refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
-import 'dart:io';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:pdf/widgets.dart' as pdfWidgets;
+import 'package:pdf/pdf.dart';
 import 'package:csv/csv.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'dart:ui' as ui;
-import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
 
-Uint8List? pngBytes;
+class PositionData {
+  final double time;
+  final double x;
+  final double y;
+  final double speed;
+
+  PositionData(this.time, this.x, this.y, this.speed);
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -16,143 +22,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Histogram Visualization',
+      title: 'Position Data Visualization',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HistogramPage(),
+      home: HomePage(),
     );
   }
 }
 
-class HistogramChart extends StatefulWidget {
-  final List<List<dynamic>> data;
-
-  const HistogramChart({Key? key, required this.data}) : super(key: key);
-
+class HomePage extends StatefulWidget {
   @override
-  _HistogramChartState createState() => _HistogramChartState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HistogramChartState extends State<HistogramChart> {
-  Uint8List? pngBytes;
+class _HomePageState extends State<HomePage> {
+  final List<PositionData> data = [];
 
   @override
   void initState() {
     super.initState();
-    createImageFromChart();
+    _loadCsvData();
   }
 
-  Future<void> createImageFromChart() async {
-    final chart = SfCartesianChart(
-      primaryXAxis: CategoryAxis(),
-      primaryYAxis: NumericAxis(),
-      series: <HistogramSeries>[
-        HistogramSeries<List<dynamic>, String>(
-          dataSource: widget.data,
-          sortFieldValueMapper: (data, _) => data[0].toString(),
-          yValueMapper: (data, _) => double.parse(data[3].toString()),
-          binInterval: 1, // Set the bin interval to 1
-        ),
-      ],
-    );
-    final pictureRecorder = ui.PictureRecorder();
-    final canvas = ui.Canvas(pictureRecorder);
-    final chartPadding = 15.0;
-    final chartHeight = 500.0; // your chart height
-    final chartWidth = 500.0; // your chart width
-
-    // chart has to be drawn to the canvaa
-
-    // store the created picture
-    final picture = pictureRecorder.endRecording();
-
-    // convert picture to image
-    final img = await picture.toImage(chartWidth.toInt(), chartHeight.toInt());
-
-    // get byte data from image
-    final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
-
-    setState(() {
-      // update pngBytes with the byte data from the image
-      pngBytes = byteData!.buffer.asUint8List();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (pngBytes == null) {
-      return CircularProgressIndicator(); // show loading indicator
-    }
-
-    return Container(
-      child: Image.memory(pngBytes!),
-    );
-  }
-}
-
-class HistogramPage extends StatefulWidget {
-  @override
-  _HistogramPageState createState() => _HistogramPageState();
-}
-
-class _HistogramPageState extends State<HistogramPage> {
-  List<List<dynamic>> csvData = [];
-  Uint8List? pngBytes;
-
-  @override
-  void initState() {
-    super.initState();
-    readCSVData();
-  }
-
-  void readCSVData() async {
-    String csvString = '''time,x,y,speed
-      1198.820,-10.950,-33.214,0.000
-      1198.840,-10.951,-33.222,0.004
-      1198.860,-10.944,-33.237,0.012
-      1198.880,-10.964,-33.260,0.02'''; // Replace with your actual CSV data
-
-    csvData = const CsvToListConverter().convert(csvString);
+  void _loadCsvData() async {
+    final csvData =
+        await rootBundle.loadString('assets/csv/34003A000C503341.csv');
+    final converter = CsvToListConverter();
+    final csvList = converter.convert(csvData);
+    data.addAll(csvList.map((row) {
+      try {
+        return PositionData(
+          double.tryParse(row[0]) ?? 0.0,
+          double.tryParse(row[1]) ?? 0.0,
+          double.tryParse(row[2]) ?? 0.0,
+          double.tryParse(row[3]) ?? 0.0,
+        );
+      } catch (e) {
+        return PositionData(
+            0.0, 0.0, 0.0, 0.0); // Provide a default value if conversion fails
+      }
+    }).toList());
     setState(() {});
-  }
-
-  SfCartesianChart pdfHistogramChart() {
-    return SfCartesianChart(
-      primaryXAxis: CategoryAxis(),
-      primaryYAxis: NumericAxis(),
-      series: <HistogramSeries>[
-        HistogramSeries<List<dynamic>, String>(
-          dataSource: csvData,
-          sortFieldValueMapper: (data, _) => data[0].toStringAsFixed(2),
-          yValueMapper: (data, _) => double.parse(data[3].toString()),
-          binInterval: 1, // Set the bin interval to 1
-        ),
-      ],
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Histogram Visualization'),
+        title: Text('Position Data Visualization'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (csvData.isNotEmpty) HistogramChart(data: csvData),
-            SizedBox(height: 20),
             Text(
-              'Average Speed: 0.01 m/s', // Replace with actual average speed
-              style: TextStyle(fontSize: 18),
+              'Average Speed Histogram',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            Image.asset('assets/logo.png'), // Replace with your image asset
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                child: charts.BarChart(
+                  _createSeriesData(),
+                  animate: true,
+                ),
+              ),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: generatePDFReport,
+              onPressed: () {
+                _generatePdfReport();
+              },
               child: Text('Generate PDF Report'),
             ),
           ],
@@ -160,51 +102,79 @@ class _HistogramPageState extends State<HistogramPage> {
       ),
     );
   }
-}
 
-void generatePDFReport() async {
-  final pdf = pw.Document();
+  List<charts.Series<PositionData, String>> _createSeriesData() {
+    final fiveMinutesData =
+        data.sublist(0, data.length > 3399 ? 3399 : data.length);
+    final tenMinutesData =
+        data.sublist(0, data.length > 6798 ? 6798 : data.length);
 
-  final image = pw.MemoryImage(pngBytes!.buffer.asUint8List());
-
-  pdf.addPage(
-    pw.Page(
-      build: (pw.Context context) {
-        return pw.Center(
-          child: pw.Column(
-            children: [
-              // add the image to the pdf
-              pw.Image(image),
-            ],
-          ),
-        );
-      },
-    ),
-  );
-
-  final output = File('report.pdf');
-  final bytes = await pdf.save();
-  await output.writeAsBytes(bytes);
-}
-
-class Histogram extends StatelessWidget {
-  final List<List<dynamic>> data;
-
-  const Histogram({Key? key, required this.data}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SfCartesianChart(
-      primaryXAxis: CategoryAxis(),
-      primaryYAxis: NumericAxis(),
-      series: <HistogramSeries>[
-        HistogramSeries<List<dynamic>, String>(
-          dataSource: data,
-          sortFieldValueMapper: (data, _) => data[0].toString(),
-          yValueMapper: (data, _) => double.parse(data[3].toString()),
-          binInterval: 1, // Set the bin interval to 1
-        ),
-      ],
+    final fiveMinutesSeries = charts.Series<PositionData, String>(
+      id: '5 Minutes',
+      domainFn: (PositionData data, _) => '5 Minutes',
+      measureFn: (PositionData data, _) => data.speed,
+      data: fiveMinutesData,
     );
+
+    final tenMinutesSeries = charts.Series<PositionData, String>(
+      id: '10 Minutes',
+      domainFn: (PositionData data, _) => '10 Minutes',
+      measureFn: (PositionData data, _) => data.speed,
+      data: tenMinutesData,
+    );
+
+    return [fiveMinutesSeries, tenMinutesSeries];
+  }
+
+  void _generatePdfReport() {
+    final pdf = pdfWidgets.Document();
+
+    pdf.addPage(
+      pdfWidgets.Page(
+        build: (context) {
+          return pdfWidgets.Column(
+            children: [
+              pdfWidgets.Text(
+                'Position Data Report',
+                style: pdfWidgets.TextStyle(fontSize: 20),
+              ),
+              pdfWidgets.SizedBox(height: 20),
+              pdfWidgets.Text(
+                'Average Speed Statistics:',
+                style: pdfWidgets.TextStyle(fontSize: 16),
+              ),
+              pdfWidgets.SizedBox(height: 10),
+              pdfWidgets.Text(
+                '5 Minutes: ...', // Add actual statistics here
+              ),
+              pdfWidgets.SizedBox(height: 10),
+              pdfWidgets.Text(
+                '10 Minutes: ...', // Add actual statistics here
+              ),
+              pdfWidgets.SizedBox(height: 20),
+              pdfWidgets.Text(
+                'Position Data:',
+                style: pdfWidgets.TextStyle(fontSize: 16),
+              ),
+              pdfWidgets.SizedBox(height: 10),
+              pdfWidgets.Table.fromTextArray(
+                data: [
+                  ['Time', 'X', 'Y', 'Speed'],
+                  ...data.map((positionData) => [
+                        positionData.time.toString(),
+                        positionData.x.toString(),
+                        positionData.y.toString(),
+                        positionData.speed.toString(),
+                      ]),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
+
+    // Save the PDF report
+    // pdf.save(); Uncomment this line to save the PDF report
   }
 }
